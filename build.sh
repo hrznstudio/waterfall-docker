5037a1f895858c79d012082b0df1df737b44ec33
Chip Wolf <hello@chipwolf.uk>
2019-08-31 21:26:22 +0100

    :sparkles: subservers

--- build.sh
+++ build.sh
@@ -3,11 +3,35 @@ set -e
 
 WATERFALL_JAR_URL="https://papermc.io/ci/job/Waterfall/lastSuccessfulBuild/artifact/Waterfall-Proxy/bootstrap/target/Waterfall.jar"
 
+SUBSERVERS_ROOT_URL="https://src.me1312.net/jenkins/job/SubServers%20Platform/lastSuccessfulBuild/artifact/Artifacts"
+SUBSERVERS_PATCHER_URL="${SUBSERVERS_PATCHER_URL}/SubServers.Patcher.sh"
+SUBSERVERS_BUNGEE_URL="${SUBSERVERS_ROOT_URL}/SubServers.Bungee.jar"
+SUBSERVERS_SYNC_URL="${SUBSERVERS_ROOT_URL}/SubServers.Sync.jar"
+
 cd /waterfall
 
 echo "Fetching Waterfall"
-wget -O waterfall.jar $WATERFALL_JAR_URL
+wget -O waterfall.jar "${WATERFALL_JAR_URL}"
 
-#echo "Fetching Plugins"
-#wget -O plugins.zip $WATERFALL_PLUGINS_ZIP_URL
+if [ ! -z $WATERFALL_PLUGINS_ZIP_URL ]; then
+  echo "Fetching Plugins"
+  wget -O plugins.zip "${WATERFALL_PLUGINS_ZIP_URL}"
+fi
+
+if [ ! -z $WATERFALL_SUBSERVERS_ROLE ]; then
+  echo "Fetching SubServers.Patcher"
+  wget -O patcher.sh "${SUBSERVERS_PATCHER_URL}"
+  chmod +x patcher.sh
+
+  if [[ "${WATERFALL_SUBSERVERS_ROLE}" == "BUNGEE" ]]; then
+    echo "Fetching SubServers.Bungee"
+    wget -O subservers.jar "${SUBSERVERS_BUNGEE_URL}"
+  elif [[ "${WATERFALL_SUBSERVERS_ROLE}" == "SYNC" ]]; then
+    echo "Fetching SubServers.Sync"
+    wget -O subservers.jar "${SUBSERVERS_SYNC_URL}"
+  fi
+
+  sh patcher.sh waterfall.jar subservers.jar
+  rm -f patcher.sh waterfall.jar subservers.jar
+fi
 
--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ set -e
 
 sed -i "s/{{motd}}/\&1${HOSTNAME}/" /waterfall/config.yml
 
-COMMAND="java -Xmx$JAVA_MEMORY -Xms$JAVA_MEMORY $JAVA_ARGS -jar waterfall.jar"
+COMMAND="java -Xmx$JAVA_MEMORY -Xms$JAVA_MEMORY $JAVA_ARGS -jar SubServers.Patched.jar"
 echo "Running Waterfall - $COMMAND"
 
 cd /waterfall
